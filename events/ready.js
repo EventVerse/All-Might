71d0ef36bedd32f2e77c261fce3a9e0194367a5e
tomu <66744640+Pitisko@users.noreply.github.com>
2020-11-23 16:59:23 +0100

    Loop statusy

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,20 @@
+const activities_list = [
+    "http://eventverse.fun", 
+    "by tomu#9436",
+    "Official EventVerse bot", 
+    "fowley je $imp"
+    ];
+
 module.exports = (client) => {
     console.log(`Ready as ${client.user.tag} to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`);
-    client.user.setPresence({
-        status: "online",
-        activity: {
-            name: "http://eventverse.fun",
-            type: "LISTENING"
-        }
-    })
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); 
+        client.user.setPresence({
+            status : "online",
+            activity : {
+                name : activities_list[index],
+                type : "COMPETING",
+            }
+        }); 
+    }, 10000); 
 };
